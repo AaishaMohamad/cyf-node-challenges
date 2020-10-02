4f5973fc05ae87373626ce57c73e59b3d4836ea8
Mawaddah <itzolda@gmail.com>
2020-10-02 09:27:34 +0100

    London mini guide level 300

--- challenge-london-mini-guide/index.js
+++ challenge-london-mini-guide/index.js
@@ -5,24 +5,65 @@ app.use(express.json());
 
 // app.use(cors());
 
-fs.readFile("data/Stratford.json", function (error, file) {
-  const parsedFile = JSON.parse(file);
+const stratford = JSON.parse(fs.readFileSync("data/Stratford.json"));
+const heathrow = JSON.parse(fs.readFileSync("data/Heathrow.json"));
+const harrow = JSON.parse(fs.readFileSync("data/Harrow.json"));
 
-  app.get("/pharmacies", (req, res) => {
-    res.send(parsedFile.pharmacies);
-  });
+app.get("/:city/pharmacies", (req, res) => {
+  let cityParam = req.params.city;
+  console.log(cityParam);
+  if (cityParam == "stratford") {
+    res.send(stratford.pharmacies);
+  }
+  if (cityParam === "heathrow") {
+    res.send(heathrow.pharmacies);
+  }
+  if (cityParam === "harrow") {
+    res.send(harrow.pharmacies);
+  }
+  res.status(404).send("Something's wrong");
+});
 
-  app.get("/colleges", (req, res) => {
-    res.send(parsedFile.colleges);
-  });
+app.get("/:city/colleges", (req, res) => {
+  let cityParam = req.params.city;
+  if (cityParam == "stratford") {
+    res.send(stratford.colleges);
+  }
+  if (cityParam === "heathrow") {
+    res.send(heathrow.colleges);
+  }
+  if (cityParam === "harrow") {
+    res.send(harrow.colleges);
+  }
+  res.status(404).send("Something's wrong");
+});
 
-  app.get("/doctors", (req, res) => {
-    res.send(parsedFile.doctors);
-  });
+app.get("/:city/doctors", (req, res) => {
+  let cityParam = req.params.city;
+  if (cityParam == "stratford") {
+    res.send(stratford.doctors);
+  }
+  if (cityParam === "heathrow") {
+    res.send(heathrow.doctors);
+  }
+  if (cityParam === "harrow") {
+    res.send(harrow.doctors);
+  }
+  res.status(404).send("Something's wrong");
+});
 
-  app.get("/hospitals", (req, res) => {
-    res.send(parsedFile.hospitals);
-  });
+app.get("/:city/hospitals", (req, res) => {
+  let cityParam = req.params.city;
+  if (cityParam == "stratford") {
+    res.send(stratford.hospitals);
+  }
+  if (cityParam === "heathrow") {
+    res.send(heathrow.hospitals);
+  }
+  if (cityParam === "harrow") {
+    res.send(harrow.hospitals);
+  }
+  res.status(404).send("Something's wrong");
 });
 
 app.get("/", (req, res) => {
